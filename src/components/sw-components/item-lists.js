6b86c388d62a8f58a747244e36268ff442f84fe2
Бутенко Владислав Александрович <buten-1@vao.obr.mos.ru>
2020-12-09 20:51:16 +0300

    up to r8 - done lesson 82

--- src/components/sw-components/item-lists.js
+++ src/components/sw-components/item-lists.js
@@ -3,12 +3,20 @@ import SwapiService from '../../services/swapi-service'
 import ItemList from '../item-list'
 import { withData } from '../hoc-helpers'
 
-const swapiService = new SwapiService
+const swapiService = new SwapiService()
 
 const {getAllPeople, getAllPlanets, getAllStarships} = swapiService
 
-const PersonList = withData(ItemList, getAllPeople)
-const StarshipList = withData(ItemList, getAllStarships)
+const WithChildFunction = (Wrapped, fn) => {
+    return (props) => {
+        return <Wrapped {...props}>{fn}</Wrapped>
+    }
+}
+const ListWithChildren = WithChildFunction(ItemList, ({name}) => <span>{name}</span>)
+const ListWithStarships = WithChildFunction(ItemList, ({model, length}) => <span>{model} - {length}</span>)
+
+const PersonList = withData(ListWithChildren, getAllPeople)
+const StarshipList = withData(ListWithStarships, getAllStarships)
 const PlanetList = withData(ItemList, getAllPlanets)
 
 export {
